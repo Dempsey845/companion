58985926e504aa4d335974fa914d6b41410e8a3b
Dempsey845 <dempseynewton1@gmail.com>
2026-06-13 21:04:30 +0100

    Added sheath/withdraw of weapons.

--- Entity/NPC/States/npc_chase_state.gd
+++ Entity/NPC/States/npc_chase_state.gd
@@ -3,33 +3,59 @@ extends State
 @export var target: Node3D
 @export var attack_distance: float = 2.0
 @export var weapon_manager: NPCWeaponManager
+@export var skeleton: HumanoidSkeleton 
 
 @onready var target_update_timer: Timer = $TargetUpdateTimer
 @onready var attack_distance_sq = attack_distance * attack_distance
 @onready var distance_check_timer: Timer = $DistanceCheckTimer
+@onready var wander_state: Node = $"../WanderState"
+
+var minimum_chase_duration: float = 5.0
+var chase_time: float = 0.0
+
+var can_attack: bool
 
 func _ready() -> void:
 	target_update_timer.timeout.connect(_on_target_update_timer_timeout)
 	distance_check_timer.timeout.connect(_on_distance_check_timer_timeout)
 
 func enter():
-	print("Entered Chase")
-	
 	if actor is not NPC:
 		push_error("This State is only compatible with NPC's!")
 		
+	chase_time = 0.0
+		
+	sheath_weapon.call_deferred()
+	
+func start_timers():
 	target_update_timer.start()
 	distance_check_timer.start()
 	
-func update(_delta: float):
+func sheath_weapon():
+	skeleton.play_upper_body_animation("Sheath", 1.2)
+	await get_tree().create_timer(0.5).timeout
+	weapon_manager.item_manager.requip_current_item(NPCItemManager.ItemSlot.RightHand)
+	can_attack = true
+	
+	start_timers.call_deferred()
+	
+func withdraw_weapon():
+	skeleton.play_upper_body_animation("Sheath", 1.2)
+	await get_tree().create_timer(0.5).timeout
+	weapon_manager.item_manager.requip_current_item(NPCItemManager.ItemSlot.Back)
+	can_attack = true
+	
+func update(delta: float):
 	actor.look_at_point(target.global_position)
+	
+	chase_time += delta
 	
 func exit():
 	actor.move = false
 	target_update_timer.stop()
 	distance_check_timer.stop()
-	weapon_manager.stop_using_weapon()
 	actor.clear_look_target()
+	withdraw_weapon()
 	
 func _on_target_update_timer_timeout():
 	actor.set_target_position(target.global_position)
@@ -39,6 +65,7 @@ func _on_distance_check_timer_timeout():
 	var distance_sq_to_target = actor.global_position.distance_squared_to(target.global_position)
 	print(distance_sq_to_target)
 	
-	if distance_sq_to_target < attack_distance_sq:
+	if distance_sq_to_target < attack_distance_sq and can_attack:
 		weapon_manager.try_use_current_weapon()
-		
+	if distance_sq_to_target > 80 and chase_time > minimum_chase_duration:
+		state_machine.change_state(wander_state)
--- Entity/NPC/npc_item_manager.gd
+++ Entity/NPC/npc_item_manager.gd
@@ -3,25 +3,42 @@ extends Node
 
 signal item_used(item_type: Item.ItemType)
 
+enum ItemSlot
+{
+	RightHand,
+	Back
+}
+
 @export var skeleton: Node3D
 @export var start_item: Item
 
-var item_slot: Node3D
 var current_world_item: WorldItem
+
+var item_slot_names: Dictionary = {
+	ItemSlot.RightHand: "RightHand",
+	ItemSlot.Back: "Back"
+}
 
 @onready var item_use_cooldown_timer: Timer = $ItemUseCooldownTimer
 
 func _ready() -> void:
-	item_slot = skeleton.get_node("Skeleton3D/ItemAttachment/ItemSlot")
-	
-	equip_new_item(start_item)
+	equip_new_item(start_item, ItemSlot.Back)
 
 func remove_current_item():
 	if current_world_item and is_instance_valid(current_world_item):
 		current_world_item.queue_free()
 
-func equip_new_item(item_resource: Item):
+func requip_current_item(item_slot_type: ItemSlot):
+	var item_slot_path = "Skeleton3D/" + item_slot_names[item_slot_type] + "ItemAttachment/ItemSlot"
+	var item_slot = skeleton.get_node(item_slot_path)
+	current_world_item.reparent(item_slot)
+	current_world_item.position = Vector3.ZERO
+	current_world_item.rotation = Vector3.ZERO
+
+func equip_new_item(item_resource: Item, item_slot_type: ItemSlot):
 	remove_current_item()
+		
+	var item_slot = skeleton.get_node("Skeleton3D/" + item_slot_names[item_slot_type] + "ItemAttachment/ItemSlot")
 		
 	current_world_item = item_resource.world_item_scene.instantiate() as WorldItem
 	current_world_item.init(item_resource)
